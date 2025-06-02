eb520aabe4fa406f0b86cf0a06b1e339bab34012
Shivam-Samant <shivamsamant000@gmail.com>
2025-06-02 20:44:11 +0530

    fix: diabetic info

--- controllers/common/patient/latestHealthData/getLatestHealthData.controller.js
+++ controllers/common/patient/latestHealthData/getLatestHealthData.controller.js
@@ -21,6 +21,7 @@ const userModel = require("../../../../models/common/user.model.js");
 const Corporate = require("../../../../models/corporates/corporate.model.js");
 const corporatePlanModel = require("../../../../models/corporates/corporatePlan.model.js");
 const Employee = require("../../../../models/patient/employee/employee.model.js");
+const LiverRiskCalculator = require("../../../../models/patient/riskAssessments/liverRiskCalculate.model.js")
 
 const healthTrackerController = {
   async getLatestHealthData(req, res) {
@@ -172,6 +173,7 @@ const healthTrackerController = {
         planFeatures,
         latestStress,
         latestDepression,
+        liverRisk
       ] = await Promise.all([
         getLatestRecord(PatientBP),
         getLatestRecord(PatientBMI),
@@ -187,6 +189,7 @@ const healthTrackerController = {
         getPlanFeatures(patientId),
         getLatestRecord(PatientStress),
         getLatestRecord(PatientDepression),
+        LiverRiskCalculator.findOne({ user: patientId }).sort({ date: -1 })
       ]);
 
       // Get corporate details if user is an employee
@@ -327,7 +330,7 @@ const healthTrackerController = {
         isDrink: latestEMR?.history?.habits?.alcohol
           ? latestEMR?.history?.habits?.alcohol
           : false,
-        isDiabetic: false,
+        isDiabetic: ['yes', 'true'].includes(liverRisk?.toLowercase()) ?? false,
         healthScore: latestHealthScore?.overallHealthScore
           ? latestHealthScore?.overallHealthScore
           : null,
